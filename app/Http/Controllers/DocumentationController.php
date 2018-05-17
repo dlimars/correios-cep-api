a23940afe8ca07d47bd206b989000c7a70ba23d6
Daniel Lima <daniel@docode.com.br>
2018-05-17 14:43:53 -0300

    Improve uses Class

--- app/Http/Controllers/DocumentationController.php
+++ app/Http/Controllers/DocumentationController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\Cep\CepRepository;
-use League\Fractal\Manager;
+use Cache;
+use Parsedown;
 
 class DocumentationController extends Controller
 {
     public function index()
     {
-        $contents = \Cache::rememberForever('documentation', function(){
+        $contents = Cache::rememberForever('documentation', function(){
             $markdown = file_get_contents(base_path('readme.md'));
-            return (new \Parsedown())->parse($markdown);
+            return (new Parsedown())->parse($markdown);
         });
 
         return view('documentation', compact('contents'));
